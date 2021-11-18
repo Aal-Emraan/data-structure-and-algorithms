0e24c4e16f1d32321b969a6ba0ebb34e0a900f54
Aal Emraan <developer.aalemraan@gmail.com>
2021-11-18 22:43:25 +0600

    bubble sort learned

---
+++ bubble_sort.js
@@ -0,0 +1,14 @@
+const numbers = [3, 25, 46, 567, 65, 76, 8, 7, 8, 87, 6789, 79, 87, 989];
+
+function sorting(array) {
+	for (let i = 0; i < array.length; i++) {
+		for (let j = 0; j < array.length; j++) {
+			if (array[j] > array[j + 1]) {
+				[array[j], array[j + 1]] = [array[j + 1], array[j]];
+			}
+		}
+	}
+	return array;
+}
+const sortedArray = sorting(numbers);
+console.log(sortedArray);
