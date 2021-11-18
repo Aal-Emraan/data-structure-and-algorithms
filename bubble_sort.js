4d4613fcddbf9611d9fe1e358aa56fcfabd84ad6
Aal Emraan <developer.aalemraan@gmail.com>
2021-11-18 22:46:24 +0600

    descending bubble sorting added

--- bubble_sort.js
+++ bubble_sort.js
@@ -4,6 +4,8 @@ function sorting(array) {
 	for (let i = 0; i < array.length; i++) {
 		for (let j = 0; j < array.length; j++) {
 			if (array[j] > array[j + 1]) {
+				// for descending order, the if statement should be
+				// if (array[j] < array[j + 1]) {
 				[array[j], array[j + 1]] = [array[j + 1], array[j]];
 			}
 		}
